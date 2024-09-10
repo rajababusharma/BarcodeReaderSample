6641f16c43673a9d9ff95215186b2842f4ba8248
Rajababu Sharma <Rajababu.Sharma@sirva.com>
2024-09-10 14:29:32 +0530

    added splash screen logo

--- BarcodeReaderSample/View/Scan.xaml.cs
+++ BarcodeReaderSample/View/Scan.xaml.cs
@@ -66,7 +66,7 @@ namespace BarcodeReaderSample.View
         private async void UpdateBarcodeInfo(string data, string symbology, DateTime timestamp)
         {
             entrysku.Text = data;
-          //  await ValidateDocket();
+            await ValidateDocket();
         }
 
         private async Task ValidateDocket()
@@ -344,7 +344,7 @@ namespace BarcodeReaderSample.View
        
         public async void OnScanButtonClicked(object sender, EventArgs args)
         {
-             await ValidateDocket();
+            // await ValidateDocket();
             if (mSelectedReader != null && mSelectedReader.IsReaderOpened)
             {
                 if (mContinuousScan)
